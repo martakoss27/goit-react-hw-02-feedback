a48de8a5dcf2a19009247b8d7b06482dfaa765c6
MartaKoss27 <27691497m@gmail.com>
2023-10-17 17:17:14 +0200

    styled-components
    Buttons

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,26 @@
+import { styled } from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 16px;
+  margin: 1em;
+  padding: 5px 15px;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+  color: #bf4f74;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
       {options.map((option, id) => {
         return (
-          <button key={id} type="button" onClick={onLeaveFeedback}>
+          <StyledButton key={id} type="button" onClick={onLeaveFeedback}>
             {option}
-          </button>
+          </StyledButton>
         );
       })}
     </>
